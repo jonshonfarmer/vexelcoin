1bcbeece73a93e2ccdd6723ce493f29b3ca4fff0
danielywwww <110917722+danielywwww@users.noreply.github.com>
2023-02-26 00:08:54 +0200

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,25 @@
+const countDownDate = new Date("Mar 23, 2023 00:00:00").getTime();
+const x = setInterval(function () {
+  let now = new Date().getTime();
+  let distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("days").innerHTML = "00";
+    document.getElementById("hours").innerHTML = "00";
+    document.getElementById("minutes").innerHTML = "00";
+    document.getElementById("seconds").innerHTML = "00";
+  }
+}, 1000);
+
+document.querySelector(".gameScreen").volume = 0.3;
